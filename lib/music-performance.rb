4230cf3725f166e003fc2a34af90273e7e4d2eac
James Tunnell <jamestunnell@gmail.com>
2014-09-20 13:14:18 -0700

    Import musicality files for computing values over time.

--- lib/music-performance.rb
+++ lib/music-performance.rb
@@ -1,3 +1,8 @@
 require 'music-transcription'
 
 require 'music-performance/version'
+
+require 'music-performance/computer/offset_limits'
+require 'music-performance/computer/linear_interpolation'
+require 'music-performance/computer/piecewise_function'
+require 'music-performance/computer/value_computer'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,5 @@
 require 'rspec'
-require 'music-transcription'
+require 'music-performance'
 
+include Music::Performance
 include Music::Transcription
-include Music::Transcription::Pitches
-
-class Samples
-  SAMPLE_PART = Part.new(
-    notes: [ Note::Quarter.new([ C1, D1 ]), Note::Quarter.new([ C2, D2 ]), Note::Whole.new([ C3, D3 ]) ],
-    dynamic_profile: Profile.new(Dynamics::P, {1.0 => Change::Immediate.new(Dynamics::MP)})
-  )
-end
