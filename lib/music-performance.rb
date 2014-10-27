027d29840e673d9a08231fe2cf854a76d28cc96f
James Tunnell <jamestunnell@gmail.com>
2014-10-27 12:10:05 -0700

    move things around

--- lib/music-performance.rb
+++ lib/music-performance.rb
@@ -2,19 +2,17 @@ require 'music-transcription'
 
 require 'music-performance/version'
 
+require 'music-performance/model/sequence'
+
 require 'music-performance/util/interpolation'
 require 'music-performance/util/piecewise_function'
 require 'music-performance/util/value_computer'
-require 'music-performance/util/note_time_converter'
-require 'music-performance/util/score_time_converter'
-require 'music-performance/util/score_collator'
 require 'music-performance/util/optimization'
 require 'music-performance/util/note_linker'
 
-require 'music-performance/sequence/attack'
-require 'music-performance/sequence/sequence'
-require 'music-performance/sequence/sequence_elements'
-require 'music-performance/sequence/glissando_converter'
-require 'music-performance/sequence/portamento_converter'
-require 'music-performance/sequence/sequence_extractor'
-
+require 'music-performance/conversion/note_time_converter'
+require 'music-performance/conversion/score_time_converter'
+require 'music-performance/conversion/score_collator'
+require 'music-performance/conversion/glissando_converter'
+require 'music-performance/conversion/portamento_converter'
+require 'music-performance/conversion/sequence_extractor'
--- lib/music-performance/sequence/attack.rb
+++
@@ -1,7 +0,0 @@
-module Music
-module Performance
-
-Attack = Struct.new(:accented)
-
-end
-end
